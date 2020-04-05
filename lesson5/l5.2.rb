191c149e4bd7383c3d2a96118b85843490947d9f
valerapol <polostyanoy@gmail.com>
2020-04-06 00:37:28 +0300

    Внесены изменения в программу 5.2

--- lesson5/l5.2.rb
+++ lesson5/l5.2.rb
@@ -1,48 +1,52 @@
-reason = ""
-while reason != "y" do
-puts "Я загадал число от 0 до 16, отгадайте какое?"
-num = rand(17)
+reason = "y"
+res1 = "Холодно. Нужно меньше."
+res2 = "Тепло. Нужно меньше."
+res3 = "Холодно. Нужно больше."
+res4 = "Тепло. Нужно больше."
+while reason == "y" do
+puts "Я загадал число от 0 до 10, отгадайте какое?"
+num = rand(11)
 puts "Первая попытка:"
 ver = gets.chomp.to_i
 if ver == num
-  puts "Молодец!!! Ты угадал с первой попытки!!! Число действительно " + num.to_s
+  abort "Молодец!!! Ты угадал с первой попытки!!! Число действительно " + num.to_s
 elsif ver > num
   if (ver - num).abs > 2
-    puts "Холодно. Нужно меньше."
+    puts res1
   else
-    puts "Тепло. Нужно меньше."
+    puts res2
   end
 else
   if (ver - num).abs > 2
-    puts "Холодно. Нужно больше."
+    puts res3
   else
-    puts "Тепло. Нужно больше."
+    puts res4
   end
 end
 puts "Вторая попытка:"
 ver = gets.chomp.to_i
 if ver == num
-  puts "Хорошо. Ты угадал со второй попытки. Число действительно " + num.to_s
+  abort "Хорошо. Ты угадал со второй попытки. Число действительно " + num.to_s
 elsif ver > num
   if (ver - num).abs > 2
-    puts "Холодно. Нужно меньше."
+    puts res1
   else
-    puts "Тепло. Нужно меньше."
+    puts res2
   end
 else
   if (ver - num).abs > 2
-    puts "Холодно. Нужно больше."
+    puts res3
   else
-    puts "Тепло. Нужно больше."
+    puts res4
   end
 end
 puts "Третья (последняя) попытка:"
 ver = gets.chomp.to_i
 if ver == num
-  puts "Ты угадал с третьей попытки. Число действительно " + num.to_s
+  abort "Ты угадал с третьей попытки. Число действительно " + num.to_s
 else
   puts "Ты не угадал. (( Число было " + num.to_s
 end
-puts "Хотите закончить? (y/n)"
+puts "Хотите попробовать еще раз? (y/n)"
 reason = gets.chomp
 end
